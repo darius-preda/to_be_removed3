3ec0f1f36c6cfe6d09f4583fc75f2af33e29aec0
Darius Preda <dariuspreda02@gmail.com>
2026-01-20 22:59:57 +0200

    Solved task 1

--- PracticalTestv03/app/src/main/java/ro/pub/cs/systems/eim/practicaltestv03/PracticalTest02.java
+++ PracticalTestv03/app/src/main/java/ro/pub/cs/systems/eim/practicaltestv03/PracticalTest02.java
@@ -1,6 +1,11 @@
 package ro.pub.cs.systems.eim.practicaltestv03;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,42 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class PracticalTest02 extends AppCompatActivity {
 
+    private EditText userInputEditText = null;
+    private Button sendButton = null;
+    private TextView descriptionTextView = null;
+
+    private void initializeViews() {
+        userInputEditText = findViewById(R.id.input_word_edit_text);
+        sendButton = findViewById(R.id.send_button);
+        descriptionTextView = findViewById(R.id.description_text_view);
+    }
+
+    private void setupListeners() {
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleSendButton();
+            }
+        });
+    }
+
+    public void handleSendButton() {
+        String userInput = userInputEditText.getText().toString();
+        if (userInput.isEmpty()) {
+            Toast.makeText(this, "Please enter a word", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.activitypractical_test02_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        initializeViews();
+        setupListeners();
     }
 }
